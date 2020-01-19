89b39f369525bbaabc774e4db042ffc54f97cffa
Jake Bacher <jakebacher@yahoo.com>
2020-01-19 09:42:53 -0500

    basic scaffolding for lineup table

--- lib/screens/lineup/lineup_screen.dart
+++ lib/screens/lineup/lineup_screen.dart
@@ -1,11 +1,27 @@
+import 'package:bigfoot_electro/theme/style.dart';
 import 'package:bigfoot_electro/widgets/bfe_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:sticky_headers/sticky_headers.dart';
 
 Widget placeholderWidget(Color color) {
    return Container(
      color: color,
    );
+}
+
+class Artist {
+  final String name;
+  const Artist({this.name});
+  //will have time at some point, and an image perhaps?
+}
+
+class _ArtistListItem extends ListTile {
+  _ArtistListItem(Artist artist)
+    : super(
+      title: new Text(artist.name),
+      subtitle: new Text(artist.name),
+      leading: new CircleAvatar(child: new Text(artist.name[0])));
 }
 
 class LineupScreen extends StatefulWidget {
@@ -15,18 +31,45 @@ class LineupScreen extends StatefulWidget {
   }
 }
 
-
+  List artistsList = <Artist> [
+    Artist(name: "Vas Majority"),
+    Artist(name: "Jake Bacher")
+  ];
 
 class _LineupState extends State<LineupScreen> {
   int _currentIndex = 0;
- final List<Widget> _children = [
+  String _title = "Schedule";
+  List artists = artistsList;
+
+  final List<Widget> _children = [
     placeholderWidget(Colors.green),
-    placeholderWidget(Colors.black)
+    ListView.builder(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      itemBuilder: (context, index){
+        return StickyHeader(
+          header: Container(
+            height: 20,
+            color: Colors.grey,
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            alignment: Alignment.centerLeft,
+            child: Text("header",
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+          content: Container(
+            color: Colors.white,
+            child: _ArtistListItem(artistsList[index]), 
+          )
+        );
+      },
+      itemCount: artistsList.length,
+    )
   ];
 
   void onTabTapped(int index) {
     setState(() {
       _currentIndex = index;
+      _title = index == 0 ? "Schedule" : "Lineup";
     });
   }
 
@@ -34,7 +77,7 @@ class _LineupState extends State<LineupScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('LINEUP'),
+        title: Text(_title),
         centerTitle: true,
       ),
       bottomNavigationBar: BottomNavigationBar(
