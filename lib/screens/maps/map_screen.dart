fb5df79d527ce13d1fdb2ea2548d3ea757386d04
Jake Bacher <jakebacher@yahoo.com>
2020-02-05 19:59:55 -0500

    map placeholder

--- lib/screens/maps/map_screen.dart
+++ lib/screens/maps/map_screen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 Widget mapScreen() {
   return Container(
-    child: Text("Maps"),
+    decoration: BoxDecoration(color: Colors.white),
+    child: Center(
+    //TODO crop right side of map images
+      child: Image.asset('assets/images/festival_map.png'),
+    ),
   );
 }
