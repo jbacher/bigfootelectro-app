73ea8989f40653c3a799be6998475f4defd50f30
Jake Bacher <jakebacher@yahoo.com>
2020-02-11 15:34:44 -0500

    big anncouncement progress

--- lib/screens/notifications/notification_screen.dart
+++ lib/screens/notifications/notification_screen.dart
@@ -12,32 +12,66 @@ class NotificationScreen extends StatefulWidget {
 
 class _NotificationState extends State<NotificationScreen> {
   
+  int _currentlySelected = -1;
+
+  void updateSelected(int selected) {
+    setState(() {
+      _currentlySelected = selected;
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         color: Colors.white
       ),
+      padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
       child: ListView.builder(
         itemCount: notifications.length,
         itemBuilder: (context, index) {
-          return Column(
-             children: <Widget>[
-               Padding(
-                padding: EdgeInsets.fromLTRB(25, 5, 25, 5),
-                child: Text(notifications[index].title,
-                  style: TextStyle(
-                    height: 1.5
-                  ) 
+          return GestureDetector(
+            onTap: () {
+
+            },
+            onTapDown: (details) { //change the color to gray
+              updateSelected(index);
+            },
+            onVerticalDragStart: (details) {
+             updateSelected(-1);
+            },
+            onTapUp: (details) {
+             updateSelected(-1);
+            },
+            child: Container(
+              decoration: new BoxDecoration (
+                color: index == _currentlySelected ? Colors.grey : Colors.white, 
+             ),
+             child: Column(
+              children: <Widget>[
+                Padding(
+                 padding: EdgeInsets.fromLTRB(25, 5, 25, 5),
+                 child: Text(notifications[index].title,
+                   style: TextStyle(
+                     height: 1.5
+                   ) 
+                 ),
                 ),
-               ),
-               Padding(
-                 padding: EdgeInsets.fromLTRB(1,5,20,5),
-                 child: Text(notifications[index].timeSent.toString()),
-               ),
-               Divider(),
-             ]
-          );
+                Container(
+                  padding: EdgeInsets.fromLTRB(25,5,20,5),
+                  alignment: Alignment.centerLeft,
+                  child: Text(notifications[index].timeSent.toString(),
+                  textAlign: TextAlign.left,
+                   style: TextStyle(
+                     color: Colors.black.withOpacity(0.6)
+                   ),
+                  ),
+                ),
+                Divider(),
+              ]
+             ),
+            ),
+          ); 
         },
       ),
     );
