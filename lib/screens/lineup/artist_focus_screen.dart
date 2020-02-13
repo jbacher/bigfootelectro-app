409f71834804084427884442866efaf9e7788b0f
Jake Bacher <jakebacher@yahoo.com>
2020-02-13 18:53:40 -0500

    big progress on lineup

--- lib/screens/lineup/artist_focus_screen.dart
+++ lib/screens/lineup/artist_focus_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class ArtistFocusScreen extends StatefulWidget {
   final Artist artist; 
 
-  const ArtistFocusScreen({this.artist});
+  const ArtistFocusScreen(this.artist);
   
   @override
   State<StatefulWidget> createState() {
@@ -17,6 +17,102 @@ class _ArtistFocusState extends State<ArtistFocusScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Text(widget.artist.name);
+    // return Text(widget.artist.name);
+    print("hithere "+widget.artist.name);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.artist.name), //im considering overlaying over picture like lost lands app
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget> [
+          Image.asset('assets/images/gorilla_face_graphic.png'),
+          Container(
+            //some padding needed
+            child: Text(widget.artist.performanceTime.toString()),
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+            // decoration: BoxDecoration(
+            //   border: Border(bottom: BorderSide())
+            // ),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text("Facebook"),
+                  trailing: Icon(Icons.navigate_next),
+                  leading: CircleAvatar(child: Image.asset('assets/images/icon_facebook.png')),
+                ),
+                Divider(),
+              ],
+            ) 
+          ),
+          Container(
+            // decoration: BoxDecoration(
+            //   border: Border(bottom: BorderSide())
+            // ),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text("Instagram"),
+                  trailing: Icon(Icons.navigate_next),
+                  leading: CircleAvatar(child: Image.asset('assets/images/icon_instagram.png')),
+                ),
+                Divider(),
+              ],
+            ) 
+          ),
+          Container(
+            // decoration: BoxDecoration(
+            //   border: Border(bottom: BorderSide())
+            // ),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text("Twitter"),
+                  trailing: Icon(Icons.navigate_next),
+                  leading: CircleAvatar(child: Image.asset('assets/images/icon_twitter.png')),
+                ),
+                Divider(),
+              ],
+            ) 
+          ),
+          Container(
+            // decoration: BoxDecoration(
+            //   border: Border(bottom: BorderSide())
+            // ),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text("SoundCloud"),
+                  trailing: Icon(Icons.navigate_next),
+                  leading: CircleAvatar(child: Image.asset('assets/images/icon_soundcloud.png')),
+                ),
+                Divider(),
+              ],
+            ) 
+          ),
+          Container(
+            // decoration: BoxDecoration(
+            //   border: Border(bottom: BorderSide())
+            // ),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text("Website"),
+                  trailing: Icon(Icons.navigate_next),
+                  leading: CircleAvatar(child: Image.asset('assets/images/icon_web.png')),
+                ),
+                Divider(),
+              ],
+            ) 
+          ),
+            ],
+          )
+        ]
+      ),
+    );
   }
 }
--- lib/screens/lineup/lineup_screen.dart
+++ lib/screens/lineup/lineup_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 
+import 'package:bigfoot_electro/screens/lineup/artist_focus_screen.dart';
 import 'package:bigfoot_electro/theme/style.dart';
 import 'package:bigfoot_electro/widgets/bfe_drawer.dart';
 import 'package:bigfoot_electro/widgets/search_bar.dart';
@@ -34,10 +35,13 @@ class LineupScreen extends StatefulWidget {
 class _LineupState extends State<LineupScreen> {
   int _currentIndex = 0;
   String _title = "Schedule";
+  
+  String _currentArtistSelected = "";
 
   List _currentList = List();
   
   Map<String, List<Artist>> _alphabeticalArtists = SplayTreeMap<String, List<Artist>>();
+  Map<DateTime, List<Artist>> _scheduleArtists = SplayTreeMap<DateTime, List<Artist>>();
 
   populateAlphbaeticalArtists() {
     artistsList.forEach((artist) {
@@ -45,15 +49,55 @@ class _LineupState extends State<LineupScreen> {
         _alphabeticalArtists[artist.name[0]] = [];
       }
       _alphabeticalArtists[artist.name[0]].add(artist);
+
+      if (!_scheduleArtists.containsKey(artist.performanceTime)) {
+        _scheduleArtists[artist.performanceTime] = [];
+      }
+      _scheduleArtists[artist.performanceTime].add(artist);
     });
+
+    
+    
     _alphabeticalArtists.forEach( (k,v) {
       v.sort((a,b) {
         return a.name.toLowerCase().compareTo(b.name.toLowerCase());
       });
+    });
 
-
+    _scheduleArtists.forEach( (k,v) {
+      v.sort((a,b) {
+        return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+      });
     });
   } 
+
+  String formatText(DateTime dateTime) {
+    String sussex = dateTime.hour < 12 ? " AM": " PM";  //check logic on this to be sure
+    String minuteText; 
+    String hourText;
+    switch (dateTime.minute) {
+      case 0:
+        minuteText = "00";
+        break;
+      default:
+        minuteText = dateTime.minute.toString(); 
+    }
+
+    switch (dateTime.hour % 12) {
+      case 0: 
+        hourText = "12";
+        break;
+      default:
+        hourText = (dateTime.hour % 12).toString(); 
+    }
+    return hourText + ":" + minuteText + sussex;
+  }
+
+  void updateSelected(String selected) {
+    setState(() {
+      _currentArtistSelected =  selected;
+    });
+  }
 
   @override initState() {
     _currentList.addAll(artistsList);
@@ -61,10 +105,8 @@ class _LineupState extends State<LineupScreen> {
     super.initState();
   }
 
-  List<Widget> _createScreens() {
-    return [
-      placeholderWidget(Colors.green),
-      Scrollbar(
+  Widget lineup() {
+    return Scrollbar(
         child: ListView.builder(
         shrinkWrap: true,
         padding: EdgeInsets.symmetric(vertical: 8.0),
@@ -87,12 +129,34 @@ class _LineupState extends State<LineupScreen> {
                 shrinkWrap: true,
                 itemCount: _alphabeticalArtists[_alphabeticalArtists.keys.elementAt(index)].length,
                 itemBuilder: (context, index2) {
-                  return Column(
+                  return Container (
+                    decoration: BoxDecoration(
+                      color: _currentArtistSelected == _alphabeticalArtists[_alphabeticalArtists.keys.elementAt(index)][index2].name ? Colors.grey : Colors.white 
+                    ),
+                    child: Column(
                     children: <Widget> [
-                      _ArtistListItem(_alphabeticalArtists[_alphabeticalArtists.keys.elementAt(index)][index2]),
+                      GestureDetector(
+                        child: _ArtistListItem(_alphabeticalArtists[_alphabeticalArtists.keys.elementAt(index)][index2]),
+                        onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ArtistFocusScreen(_alphabeticalArtists[_alphabeticalArtists.keys.elementAt(index)][index2])
+                          ));
+                        },
+                        onTapDown: (details) { //change the color to gray
+                          updateSelected(_alphabeticalArtists[_alphabeticalArtists.keys.elementAt(index)][index2].name);
+                        },
+                        onVerticalDragStart: (details) {
+                          updateSelected("");
+                        },
+                        onTapUp: (details) {
+                          updateSelected("");
+                        },
+                      ),
                       index2 <  _alphabeticalArtists[_alphabeticalArtists.keys.elementAt(index)].length -1 ? Divider() : SizedBox.shrink() 
                     ]
-                  );
+                  ),
+                  );      
                 },
               ), 
             )
@@ -100,8 +164,69 @@ class _LineupState extends State<LineupScreen> {
         },
         itemCount: _alphabeticalArtists.keys.length,
         ),
-      )
-    ];
+      );
+  }
+
+  Widget schedule() {
+    return Scrollbar(
+        child: ListView.builder(
+        shrinkWrap: true,
+        padding: EdgeInsets.symmetric(vertical: 8.0),
+        itemBuilder: (context, index){
+          return StickyHeader(
+            header: Container(
+              height: 20,
+              color: Colors.grey, //pick a lighter gray to match bottom bar
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              alignment: Alignment.centerLeft,
+              child: Text(formatText(_scheduleArtists.keys.elementAt(index)),
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+            content: Container(
+              color: Colors.white,
+              // child: _ArtistListItem(artistsList[index]),
+              child: ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: _scheduleArtists[_scheduleArtists.keys.elementAt(index)].length,
+                itemBuilder: (context, index2) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: _currentArtistSelected == _scheduleArtists[_scheduleArtists.keys.elementAt(index)][index2].name ? Colors.grey : Colors.white
+                    ),
+                    child: Column(
+                    children: <Widget> [
+                      GestureDetector(
+                        child: _ArtistListItem(_scheduleArtists[_scheduleArtists.keys.elementAt(index)][index2]),
+                        onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ArtistFocusScreen(_scheduleArtists[_scheduleArtists.keys.elementAt(index)][index2])
+                          ));
+                        },
+                        onTapDown: (details) { //change the color to gray
+                          updateSelected(_scheduleArtists[_scheduleArtists.keys.elementAt(index)][index2].name);
+                        },
+                        onVerticalDragStart: (details) {
+                          updateSelected("");
+                        },
+                        onTapUp: (details) {
+                          updateSelected("");
+                        },
+                      ),
+                      index2 <  _scheduleArtists[_scheduleArtists.keys.elementAt(index)].length -1 ? Divider() : SizedBox.shrink() 
+                    ]
+                  ),
+                  );
+                },
+              ), 
+            )
+          );
+        },
+        itemCount: _scheduleArtists.keys.length,
+        ),
+      );
   }
 
   void onTabTapped(int index) {
@@ -139,7 +264,7 @@ class _LineupState extends State<LineupScreen> {
             //TODO: search bar might not be needed as the plan is to have alphabetical scrolling!
             searchBar(null),
             Expanded(
-              child: _createScreens()[_currentIndex],
+              child: _currentIndex == 0 ? schedule() : lineup(),
             )
           ],
         ) 
